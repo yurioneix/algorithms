f9cb30ad549467eb5e78c4f035c47c624ab03b97
yurioneix <yurioneix18@gmail.com>
2023-07-06 17:22:36 -0300

    Requisito 1: resolvido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    counter = 0
+
+    for begin, end in permanence_period:
+
+        if not isinstance(begin, int) or not isinstance(end, int):
+            return None
+
+        if target_time in range(begin, end + 1):
+            counter += 1
+
+    return counter
