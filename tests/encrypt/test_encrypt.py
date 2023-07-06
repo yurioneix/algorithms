e1b0b66cadff105812a636718a6059bb1447805f
yurioneix <yurioneix18@gmail.com>
2023-07-06 18:38:41 -0300

    Requisito 2: resolvido

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    "Ao passar um tipo invalido como parametro, retorna um erro"
+    with pytest.raises(TypeError):
+        encrypt_message("german", "cano")
+
+    "Ao passar uma key que nao existe na string, retorna a string invertida"
+    assert encrypt_message("fluminense", 12) == "esnenimulf"
+
+    "Retorna a string invertida na parte um e dois concatenados por _"
+    assert encrypt_message("fluminense", 5) == "imulf_esnen"
